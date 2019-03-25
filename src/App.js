a6c290a2dac1795db0258e2d644303a8717a8eda
Andres Bolanos <abolanos@cambriasolutions.com>
2019-03-25 13:40:10 -0700

    Updated testing scripts

--- src/App.js
+++ src/App.js
@@ -13,41 +13,4 @@ class App extends Component {
   }
 }
 
-/*
-    STATE:
-        filterRange: { startDate: '21-03-2019', endDate: '22-03-2019'},
-        conversationsTotal: 0,
-        durationTotal: 0,
-        intents: [ { id: '1234', name: 'support', occurrences: 3, sessions: 9 } ],
-        exitIntents: [ { id: '1234', name: 'welcome', exits: 2 } ],
-        supportRequests: 0
-
-    ACTIONS:
-        - Update filter date range
-        - Q1: Get all conversations in date range
-            - Sum all conversation durations
-            - Get list of exitIntents and counters
-            - Get support requests from Q1
-        - Q2: Get all intents in date range from metrics collection
-
-    Queries: 
-      - Q1: Get all conversations in date range
-      - Q2: Get all conversations with hasSupportRequest == true
-      - Q3: Starting on Q1 
-
-      Metrics based on queries:
-      - Users Metric (amount of conversations):
-        - Q1 count
-      - Conversation Duration:
-        - Loop through Q1, store sum of duration / amount of conversations
-      - Frequent Intents:
-        - On Q1
-      - Support requests:
-        Loop through Q2
-        - Frequency:
-          - On the same Q2 loop, store a count of support requests
-        - Percentage of conversations with support requests:
-          - Count of conversations with hasSupportRequest(true) / amount of conversations
-  */
-
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { createRenderer } from 'react-test-renderer/shallow'
+import App from './App'
+import Header from './containers/Header'
+import Dashboard from './containers/Dashboard'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const setup = propOverrides => {
+  const renderer = createRenderer()
+  renderer.render(<App />)
+  const output = renderer.getRenderOutput()
+  return output
+}
+
+describe('components', () => {
+  describe('Header', () => {
+    it('should render', () => {
+      const output = setup()
+      const [header] = output.props.children
+      expect(header.type).toBe(Header)
+    })
+  })
+
+  describe('Dashboard', () => {
+    it('should render', () => {
+      const output = setup()
+      const [, mainSection] = output.props.children
+      expect(mainSection.type).toBe(Dashboard)
+    })
+  })
+})
