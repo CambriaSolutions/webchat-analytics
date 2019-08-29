9b815c92637702224dc0e6b1cc5fc9c294f5617a
Chris Freeman <cc.freem88@gmail.com>
2019-08-29 10:57:51 -0700

    Script to update old metrics collections (#47)
    * Add logic for saving number of support requests per day
    
    * wip for exit intents consolidation
    
    * refactor exit intent counter
    
    * refactor conversations with requests
    
    * refactor end of conversation collection
    
    * add numConversations and start work on conversation duration
    
    * initial brainstorm of conversation duration
    
    * reference number of conversations only with duration
    
    * uncomment storage code and remove logs
    
    * initial working solution
    
    * add refactoring the exit intents
    
    * add comments
    
    * update comments
    
    * move to scripts directory, add date correction logic
    
    * Revert "move to scripts directory, add date correction logic"
    
    This reverts commit 8ce31c5846aa2994090e61ff3c9b34aaaa1a9383.
    
    * update reversion
    
    * update package.json
    
    * update destination and rename keyfile
    
    * remove yarn lock and duplicate calcMetric
    
    * move fb config to env

---
+++ scripts/combData.js
@@ -0,0 +1,190 @@
+require('dotenv').config()
+const admin = require('firebase-admin')
+const format = require('date-fns/format')
+const parse = require('date-fns/parse')
+const fs = require('fs')
+
+const serviceAccount = require('./analyticsKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+})
+
+const db = admin.firestore()
+
+let conversationsRef = db.collection(
+  `projects/${process.env.FIREBASE_PROJECT_ID}/conversations/`
+)
+
+const performQuery = async (startDate, endDate) => {
+  let dailyMetric = {}
+  let daysToInspect = []
+
+  await conversationsRef
+    .where('createdAt', '>=', startDate)
+    .where('createdAt', '<=', endDate)
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        let numConversations = 0
+        let numConversationsWithDuration = 0
+        let numConversationsWithSupportRequests = 0
+        let conversationDuration = 0
+        let exitIntents = {}
+
+        const data = doc.data()
+
+        const day = format(data.createdAt.toDate(), 'MM-DD-YYYY')
+
+        // // Make a new day in the daily metric
+        if (dailyMetric.hasOwnProperty(day)) {
+          // Increment number of conversations
+          dailyMetric[day].numConversations += 1
+
+          // If the conversation has a duration, increment number of
+          // conversations with a duration (engaged users) and add the
+          // duration to the total duration for the day
+          // if (data.duration && data.duration > 0) {
+          if (data.duration && data.duration > 0 && data.duration < 86400) {
+            // Used for average duration logic
+            dailyMetric[day].conversationDuration += data.duration
+            // Used for engaged users
+            dailyMetric[day].numConversationsWithDuration += 1
+          }
+
+          // If the duration of the conversation is longer than 1 day
+          // save to file to investigate later
+          if (data.duration > 86400) {
+            daysToInspect.push({
+              day,
+              conversationId: doc.id,
+              duration: data.duration,
+              hours: Math.round(data.duration / 3600),
+            })
+          }
+
+          // Increment number of support requests
+          if (data.hasSupportRequest) {
+            dailyMetric[day].numConversationsWithSupportRequests += 1
+          }
+
+          // Increment exit intents per conversation
+          if (data.lastIntent) {
+            dailyMetric[day].exitIntents[doc.id] = data.lastIntent
+          }
+        } else {
+          // Increment number of conversations
+          numConversations += 1
+
+          // If the conversation has a duration, increment number of
+          // conversations with a duration (engaged users) and add the
+          // duration to the total duration for the day
+          if (data.duration && data.duration > 0) {
+            conversationDuration += data.duration
+            numConversationsWithDuration += 1
+          }
+
+          // Increment the number of conversations with support requests counter
+          if (data.hasSupportRequest) {
+            numConversationsWithSupportRequests += 1
+          }
+
+          // Increment exit intents per conversation
+          if (data.lastIntent) {
+            exitIntents[doc.id] = data.lastIntent
+          }
+
+          dailyMetric[day] = {
+            numConversations,
+            numConversationsWithDuration,
+            numConversationsWithSupportRequests,
+            conversationDuration,
+            exitIntents,
+          }
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  // Save wildcard conversation lengths to json for later investigation
+  fs.writeFile('./daysToInspect.json', JSON.stringify(daysToInspect), err => {
+    if (err) throw err
+    console.log('Saved!')
+  })
+  return dailyMetric
+}
+
+const start = new Date('Jun 10 2019 00:00:00 GMT-0700 (Pacific Daylight Time)')
+const end = new Date('Aug 28 2019 00:00:00 GMT-0700 (Pacific Daylight Time)')
+
+// Perform a query with start and end
+performQuery(start, end).then(metric => {
+  // Create a reference for the metrics
+  const metricsRef = db.collection(`projects/mdhs-csa-dev/metrics`)
+  // Strip conversations from intents
+  for (const day in metric) {
+    const dayMetric = metricsRef.doc(day)
+
+    // Format exit intents
+    const exitIntents = metric[day].exitIntents
+    let newExitIntents = []
+    for (const intent in exitIntents) {
+      const currentIntent = exitIntents[intent].name
+      // Check to see if this intent is already on the list
+      const exitIntentExists = newExitIntents.filter(
+        intent => intent.name === currentIntent
+      )[0]
+      if (exitIntentExists) {
+        exitIntentExists.occurrences++
+      } else {
+        const newExitIntent = {
+          name: exitIntents[intent].name,
+          id: exitIntents[intent].id,
+          occurrences: 1,
+        }
+        newExitIntents.push(newExitIntent)
+      }
+    }
+
+    let newDate
+    let intents
+    dayMetric
+      .get()
+      .then(doc => {
+        const data = doc.data()
+
+        // Pull out the intents, and strip conversations
+        if (data) {
+          data.intents.map(intent => {
+            delete intent.conversations
+          })
+        }
+
+        // Ensure that the date in the database matches the document id
+        newDate = admin.firestore.Timestamp.fromDate(parse(doc.id))
+        intents = data.intents
+      })
+      .then(() => {
+        const {
+          numConversations,
+          numConversationsWithDuration,
+          numConversationsWithSupportRequests,
+          conversationDuration,
+        } = metric[day]
+        const averageConversationDuration = Math.round(
+          conversationDuration / numConversationsWithDuration
+        )
+        dayMetric.update({
+          averageConversationDuration,
+          exitIntents: newExitIntents,
+          numConversationsWithDuration,
+          numConversationsWithSupportRequests,
+          intents,
+          date: newDate,
+          numConversations,
+        })
+      })
+  }
+})
