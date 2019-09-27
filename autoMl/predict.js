d59f53b7be6dfdf648a45644d347d60d6cbd3e8b
Chris Freeman <cc.freem@yahoo.com>
2019-09-27 10:11:10 -0700

    restructure folder

--- autoMl/predict.js
+++ autoMl/predict.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const fs = require('fs')
 const automl = require('@google-cloud/automl')
 const client = new automl.v1beta1.PredictionServiceClient({})
-// const queries = require('./dataFiles_raw/fallbackFullDetails_06-01_07-01')
+const queries = require('./dataFiles_raw/fallbackFullDetails_06-01_07-01')
 // const queries = require('./dataFiles_raw/fallbackFullDetails_07-01_08-01')
 // const queries = require('./dataFiles_raw/fallbackFullDetails_08-01_09-01')
-const queries = require('./dataFiles_raw/fallbackFullDetails_09-01_10-01')
+// const queries = require('./dataFiles_raw/fallbackFullDetails_09-01_10-01')
 
 const admin = require('firebase-admin')
 const { format } = require('date-fns')
@@ -20,9 +20,9 @@ const getIdFromPath = path => /[^/]*$/.exec(path)[0]
 
 const db = admin.firestore()
 const start = new Date(
-  'Sept 21 2019  00:00:00 GMT-0700 (Pacific Daylight Time)'
+  'Sept 27 2019  00:00:00 GMT-0700 (Pacific Daylight Time)'
 )
-const end = new Date('Sept 24 2019 00:00:00 GMT-0700 (Pacific Daylight Time)')
+const end = new Date('Sept 28 2019 00:00:00 GMT-0700 (Pacific Daylight Time)')
 const startDate = format(start, 'MM-DD')
 const endDate = format(end, 'MM-DD')
 const intent = {
@@ -126,61 +126,65 @@ const predict = async intentDetails => {
   })
 
   fs.writeFile(
-    `./predictionAggregate_09-01_10-01.json`,
+    `./predictions/predictionAggregate_${format(start, 'MM-DD')}-${format(
+      start,
+      'MM-DD'
+    )}.json`,
     JSON.stringify(sortedAggregate),
     err => {
       if (err) throw err
       console.log('Saved!')
     }
   )
-  // return (predictions = { predictionsWithQuery, predictionAggregate })
 }
-
 predict(queries)
-// const performQuery = (start, end, intent) => {
-//   db.collection(`projects/${process.env.FIREBASE_PROJECT_ID}/requests`)
-//     .where('createdAt', '>', start)
-//     .where('createdAt', '<', end)
-//     .where('intentId', '==', intent.id)
-//     .orderBy('createdAt', 'desc')
-//     .get()
-//     .then(querySnapshot => {
-//       let intentDetails = []
-//       querySnapshot.forEach(doc => {
-//         let tempData = doc.data()
-//         intentDetails.push({
-//           // Save the detection of confidence
-//           intentDetectionConfidence:
-//             tempData.queryResult.intentDetectionConfidence,
-//           // Save the actual text
-//           messageText: tempData.queryResult.queryText,
-//           // Save the output context
-//           outputContexts: tempData.queryResult.outputContexts
-//             ? tempData.queryResult.outputContexts.map(o => ({
-//                 ...o,
-//                 context: getIdFromPath(o.name),
-//               }))
-//             : [],
-//           // Save the conversation Id in case we want to investigate
-//           conversationId: getIdFromPath(tempData.session),
-//           // Save when the conversation is created
-//           createdAt: format(tempData.createdAt.toDate(), 'MM-DD-YYYY'),
-//         })
-//       })
-//       return intentDetails
-//     })
-//     .then(intentDetails => predict(intentDetails))
-//     .then(predictions => {
-//       // Save predictions
-//       fs.writeFile(
-//         `./predictions_${start}-${end}.json`,
-//         JSON.stringify(predictions),
-//         err => {
-//           if (err) throw err
-//           console.log('Saved!')
-//         }
-//       )
-//     })
-// }
+const performQuery = (start, end, intent) => {
+  db.collection(`projects/${process.env.FIREBASE_PROJECT_ID}/requests`)
+    .where('createdAt', '>', start)
+    .where('createdAt', '<', end)
+    .where('intentId', '==', intent.id)
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(querySnapshot => {
+      let intentDetails = []
+      querySnapshot.forEach(doc => {
+        let tempData = doc.data()
+        intentDetails.push({
+          // Save the detection of confidence
+          intentDetectionConfidence:
+            tempData.queryResult.intentDetectionConfidence,
+          // Save the actual text
+          messageText: tempData.queryResult.queryText,
+          // Save the output context
+          outputContexts: tempData.queryResult.outputContexts
+            ? tempData.queryResult.outputContexts.map(o => ({
+                ...o,
+                context: getIdFromPath(o.name),
+              }))
+            : [],
+          // Save the conversation Id in case we want to investigate
+          conversationId: getIdFromPath(tempData.session),
+          // Save when the conversation is created
+          createdAt: format(tempData.createdAt.toDate(), 'MM-DD-YYYY'),
+        })
+      })
+      return intentDetails
+    })
+    .then(intentDetails => predict(intentDetails))
+  // .then(predictions => {
+  //   // Save predictions
+  //   fs.writeFile(
+  //     `./predictions_${format(start, 'MM-DD-YYYY')}-${format(
+  //       start,
+  //       'MM-DD-YYYY'
+  //     )}.json`,
+  //     JSON.stringify(predictions),
+  //     err => {
+  //       if (err) throw err
+  //       console.log('Saved!')
+  //     }
+  //   )
+  // })
+}
 
 // performQuery(start, end, intent)
