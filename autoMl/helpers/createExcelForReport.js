613a9cb6506fe3c8c4c40725b1e22c03331d70b5
Chris Freeman <cc.freem@yahoo.com>
2019-10-03 17:19:46 -0700

    create one excel workbook with each category as a sheet

--- autoMl/helpers/createExcelForReport.js
+++ autoMl/helpers/createExcelForReport.js
@@ -2,15 +2,71 @@ const Excel = require('exceljs')
 const workbook = new Excel.Workbook()
 const aggregate = require('../predictions/predictionAggregate_06-01-10-01.json')
 
+const cellDictionary = {
+  occurences: 'A',
+  queries: 'B',
+  contextsName: 'C',
+  contextsCount: 'D',
+}
+
 for (const key in aggregate) {
   workbook.addWorksheet(key)
   const currentSheet = workbook.getWorksheet(key)
+
+  // Create columns
   currentSheet.columns = [
-    { header: 'occurences', key: 'occurences', width: 10 },
-    { header: 'queries', key: 'queries', width: 32 },
-    { header: 'Context Name', key: 'contextsName' },
-    { header: 'Context Count', key: 'contextsCount' },
+    {
+      header: 'Occurences',
+      key: 'occurences',
+      width: 12,
+      style: {
+        alignment: {
+          vertical: 'middle',
+          horizontal: 'center',
+        },
+      },
+    },
+    {
+      header: 'Queries',
+      key: 'queries',
+      width: 40,
+    },
+    {
+      header: 'Context Name',
+      key: 'contextsName',
+      width: 40,
+    },
+    {
+      header: 'Context Count',
+      key: 'contextsCount',
+      width: 14,
+      style: {
+        alignment: {
+          vertical: 'middle',
+          horizontal: 'center',
+        },
+      },
+    },
   ]
+
+  // Format header cells
+  const headerCells = ['A1', 'B1', 'C1', 'D1']
+  headerCells.forEach(cell => {
+    const currentCell = currentSheet.getCell(cell)
+    currentCell.alignment = {
+      vertical: 'middle',
+      horizontal: 'center',
+    }
+    currentCell.border = {
+      top: { style: 'thin' },
+      left: { style: 'thin' },
+      bottom: { style: 'thin' },
+      right: { style: 'thin' },
+    }
+    currentCell.font = {
+      bold: true,
+    }
+  })
 
   // Add occurences
   currentSheet.addRow({
@@ -18,19 +74,20 @@ for (const key in aggregate) {
   })
 
   // Add queries
-  aggregate[key].queries.forEach(query => {
-    currentSheet.addRow({
-      queries: query,
-    })
+  aggregate[key].queries.forEach((query, i) => {
+    const queryCell = `${cellDictionary['queries']}${i + 2}`
+    const currentCell = currentSheet.getCell(queryCell)
+    currentSheet.alignment = { wrapText: true }
+    currentCell.value = query
   })
 
   // Add contexts
-  aggregate[key].contexts.forEach(context => {
-    currentSheet.addRow({
-      contextsName: context.name,
-      contextsCount: context.count,
-    })
+  aggregate[key].contexts.forEach((context, i) => {
+    const contextNameCell = `${cellDictionary['contextsName']}${i + 2}`
+    const contextCountCell = `${cellDictionary['contextsCount']}${i + 2}`
+    currentSheet.getCell(contextNameCell).value = context.name
+    currentSheet.getCell(contextCountCell).value = context.count
   })
 }
 
-workbook.xlsx.writeFile('./filename.xlsx')
+workbook.xlsx.writeFile('./fileAggregate.xlsx')
