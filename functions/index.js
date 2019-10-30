6bb78e09a1a83c5ecfb5e325a3926b67f46f2f47
Jade Tran <jtran@cambriasolutions.com>
2019-10-30 11:51:04 -0700

    event listener to train agent for new phrases

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const cors = require('cors')({ origin: true })
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const dialogflow = require('dialogflow')
+
+const intentsClient = new dialogflow.IntentsClient()
+
 admin.initializeApp(functions.config().firebase)
 
 // Google Cloud Storage Setup
@@ -694,3 +698,64 @@ exports.downloadExport = functions.https.onRequest((req, res) => {
       })
   })
 })
+
+exports.trainModels = functions.firestore
+  .document('/projects/mdhs-csa-dev/test/{testId}')
+  .onUpdate(async (change, context) => {
+    const afterUpdateFields = change.after.data()
+    const occurrences = afterUpdateFields.occurrences
+    const phrase = afterUpdateFields.phrase
+    if (occurrences === 10) {
+      // train agent
+      try {
+        let intent = await getIntent(
+          'projects/mdhs-csa-dev-beta/agent/intents/2b8c862a-4607-40ac-9ad0-2e01171a3b93' // TODO get intent id from db
+        )
+        let trainingPhrase = {
+          parts: [
+            {
+              text: phrase,
+            },
+          ],
+        }
+        intent.trainingPhrases.push(trainingPhrase)
+        try {
+          let updatedIntent = await updateIntent(intent)
+          console.log('Updated intent with new training phrase.')
+        } catch (e) {
+          console.log('Unable to update intent')
+          console.log(e)
+        }
+      } catch (err) {
+        console.log('Unable to get intent')
+        console.log(err)
+      }
+      // train category model
+      // train subject matter model
+    }
+    return afterUpdateFields.occurrences
+  })
+
+async function getIntent(intentId) {
+  try {
+    let responses = await intentsClient.getIntent({
+      name: intentId,
+      intentView: 'INTENT_VIEW_FULL',
+    })
+    const response = responses[0]
+    return response
+  } catch (err) {
+    return err
+  }
+}
+async function updateIntent(intent) {
+  const request = {
+    intent: intent,
+    intentView: 'INTENT_VIEW_FULL',
+  }
+  try {
+    return (responses = await intentsClient.updateIntent(request))
+  } catch (err) {
+    return err
+  }
+}
