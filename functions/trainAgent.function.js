e05690db297c686896ffed0cbcc50b9c0f7ff05f
Jade Tran <jtran@cambriasolutions.com>
2019-11-19 10:48:27 -0800

    added comments to trainAgent function

--- functions/trainAgent.function.js
+++ functions/trainAgent.function.js
@@ -7,6 +7,10 @@ const store = admin.firestore()
 
 const intentsClient = new dialogflow.IntentsClient()
 
+/**
+ * Trigger function on 'queriesForTraining' collection updates
+ * to determine occurrence threshold for DF agent training
+ */
 exports = module.exports = functions.firestore
   .document(`/projects/${process.env.AGENT_PROJECT}/queriesForTraining/{id}`)
   .onUpdate(async (change, context) => {
@@ -14,17 +18,21 @@ exports = module.exports = functions.firestore
     const afterUpdateFields = change.after.data()
     const agentTrained = afterUpdateFields.agentTrained
     const occurrences = afterUpdateFields.occurrences
-
     const phrase = afterUpdateFields.phrase
     const intentId = afterUpdateFields.intent.id
-
+    // If occurrences reaches 10 and agent is not trained
     if (occurrences === 10 && agentTrained === false) {
-      // train DF agent
       await trainAgent(phrase, intentId, docId)
     }
     return afterUpdateFields
   })
 
+/**
+ * Train DF agent
+ * @param {*} phrase user phrase from db
+ * @param {*} intentId matched intent id
+ * @param {*} docId document id in firebase
+ */
 async function trainAgent(phrase, intentId, docId) {
   try {
     let intent = await getIntent(
@@ -37,10 +45,13 @@ async function trainAgent(phrase, intentId, docId) {
         },
       ],
     }
+    // add new training phrase in the intent list of training phrases
     intent.trainingPhrases.push(trainingPhrase)
     try {
+      // updates the intent with intent object containing the new training phrase
       let updatedIntent = await updateIntent(intent)
       console.log('Updated intent with new training phrase.')
+      // set agentTrained to true after we updated the intent
       await store
         .collection(
           `/projects/${process.env.AGENT_PROJECT}/queriesForTraining/`
@@ -55,6 +66,10 @@ async function trainAgent(phrase, intentId, docId) {
   }
 }
 
+/**
+ * Get DF intent data by intent id
+ * @param {*} intentId
+ */
 async function getIntent(intentId) {
   try {
     let responses = await intentsClient.getIntent({
@@ -67,6 +82,10 @@ async function getIntent(intentId) {
     return err
   }
 }
+/**
+ * Update DF agent
+ * @param {*} intent
+ */
 async function updateIntent(intent) {
   const request = {
     intent: intent,
