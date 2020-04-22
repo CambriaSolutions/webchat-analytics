fbcdab519a0299f0fc2d81d2dde5aa77c26584fb
Bryand Acosta <acostab@infinity-software.com>
2020-04-22 10:17:27 -0400

    Removing some code so downloadExport.function connects using default credentials

--- functions/downloadExport.function.js
+++ functions/downloadExport.function.js
@@ -4,13 +4,7 @@ const cors = require('cors')({ origin: true })
 
 // Google Cloud Storage Setup
 const { Storage } = require('@google-cloud/storage')
-const storage = new Storage({
-  projectId: process.env.GCS_PROJECT_ID,
-  credentials: {
-    private_key: process.env.GCS_PRIVATE_KEY.replace('/\\n/g', '\n'),
-    client_email: process.env.GCS_CLIENT_EMAIL,
-  },
-})
+const storage = new Storage()
 const bucketName = 'daily-json-exports'
 
 // Calculate metrics based on requests
