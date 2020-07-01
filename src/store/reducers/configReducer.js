39d890c107a4bef457fea70d577e11f46c8a94ad
Bryand Acosta <acostab@infinity-software.com>
2020-07-01 14:05:56 -0400

    Removed a utility function that was not necessary

--- src/store/reducers/configReducer.js
+++ src/store/reducers/configReducer.js
@@ -1,11 +1,9 @@
 import * as actionTypes from '../actions/actionTypes'
-import { updateObject } from '../utility'
 
 import { subDays } from 'date-fns'
 
 const initialState = {
   projects: [],
-  defaultProject: '',
   loading: false,
   downloadExportDate: subDays(new Date(), 1),
   showSettings: false,
@@ -17,82 +15,86 @@ const initialState = {
   updateRealtime: true,
 }
 
-const fetchProjectsStart = (state, action) => {
-  return updateObject(state, { loading: true })
+const fetchProjectsStart = (state) => {
+  return { ...state, loading: true }
 }
 
 const fetchProjectsSuccess = (state, action) => {
-  return updateObject(state, {
+  return {
+    ...state,
     projects: action.projects,
     loading: false,
-  })
+  }
 }
 
-const fetchProjectsFail = (state, action) => {
-  return updateObject(state, { loading: false })
+const fetchProjectsFail = (state) => {
+  return { ...state, loading: false }
 }
 
-const fetchIntentDetailsStart = (state, action) => {
-  return updateObject(state, { loadingIntentDetails: true })
+const fetchIntentDetailsStart = (state) => {
+  return { ...state, loadingIntentDetails: true }
 }
 
 const fetchIntentDetailsSuccess = (state, action) => {
-  return updateObject(state, {
+  return {
+    ...state,
     intentDetails: action.intentDetails,
     loadingIntentDetails: false,
-  })
+  }
 }
 
-const fetchIntentDetailsFail = (state, action) => {
-  return updateObject(state, { loadingIntentDetails: false })
+const fetchIntentDetailsFail = (state) => {
+  return { ...state, loadingIntentDetails: false }
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     // Projects
     case actionTypes.FETCH_PROJECTS_START:
-      return fetchProjectsStart(state, action)
+      return fetchProjectsStart(state)
     case actionTypes.FETCH_PROJECTS_SUCCESS:
       return fetchProjectsSuccess(state, action)
     case actionTypes.FETCH_PROJECTS_FAIL:
-      return fetchProjectsFail(state, action)
+      return fetchProjectsFail(state)
     // Intent Details
     case actionTypes.FETCH_INTENT_DETAILS_START:
-      return fetchIntentDetailsStart(state, action)
+      return fetchIntentDetailsStart(state)
     case actionTypes.FETCH_INTENT_DETAILS_SUCCESS:
       return fetchIntentDetailsSuccess(state, action)
     case actionTypes.FETCH_INTENT_DETAILS_FAIL:
-      return fetchIntentDetailsFail(state, action)
+      return fetchIntentDetailsFail(state)
     // Config settings
     case actionTypes.UPDATE_EXPORT_DATE:
-      return updateObject(state, {
+      return {
+        ...state,
         downloadExportDate: action.downloadExportDate,
-      })
-    case actionTypes.UPDATE_DEFAULT_PROJECT:
-      return updateObject(state, {
-        defaultProject: action.defaultProject,
-      })
+      }
     case actionTypes.TOGGLE_SETTINGS:
-      return updateObject(state, {
+      return {
+        ...state,
         showSettings: action.showSettings,
-      })
+      }
     case actionTypes.TOGGLE_INTENT_MODAL:
-      return updateObject(state, {
+      return {
+        ...state,
         showIntentModal: action.showIntentModal,
-      })
+      }
     case actionTypes.TOGGLE_CONFIG_LOADING:
-      return updateObject(state, {
+      return {
+        ...state,
         loading: action.loading,
-      })
+      }
     case actionTypes.CLOSE_SNACKBAR:
-      return updateObject(state, {
+      return {
+        ...state,
         snackbarOpen: false,
-      })
+      }
     case actionTypes.SHOW_SNACKBAR:
-      return updateObject(state, {
+      return {
+        ...state,
         snackbarOpen: true,
         snackbarMessage: action.message,
-      })
+      }
     default:
       return state
   }
--- src/store/reducers/metricsReducer.js
+++ src/store/reducers/metricsReducer.js
@@ -1,5 +1,4 @@
 import * as actionTypes from '../actions/actionTypes'
-import { updateObject } from '../utility'
 
 const initialState = {
   intents: [], // { id: '1234', name: 'support', occurrences: 3, sessions: 9 }
@@ -17,11 +16,12 @@ const initialState = {
 }
 
 const fetchMetricsStart = (state, action) => {
-  return updateObject(state, { loading: true })
+  return { ...state, loading: true }
 }
 
 const fetchMetricsSuccess = (state, action) => {
-  return updateObject(state, {
+  return {
+    ...state,
     intents: action.intents,
     supportRequests: action.supportRequests,
     supportRequestTotal: action.supportRequestTotal,
@@ -34,18 +34,19 @@ const fetchMetricsSuccess = (state, action) => {
     durationTotal: action.durationTotal,
     durationTotalNoExit: action.durationTotalNoExit,
     exitIntents: action.exitIntents,
-  })
+  }
 }
 
 const fetchMetricsFail = (state, action) => {
-  return updateObject(state, { loading: false })
+  return { ...state, loading: false }
 }
 
 const updateFeedbackType = (state, action) => {
-  return updateObject(state, {
+  return {
+    ...state,
     feedbackSelected: action.feedbackType,
     feedbackFiltered: action.feedbackFiltered,
-  })
+  }
 }
 
 const reducer = (state = initialState, action) => {
@@ -59,7 +60,8 @@ const reducer = (state = initialState, action) => {
     case actionTypes.UPDATE_FEEDBACK_TYPE:
       return updateFeedbackType(state, action)
     case actionTypes.UPDATE_METRICS:
-      return updateObject(state, {
+      return {
+        ...state,
         intents: action.intents,
         supportRequests: action.supportRequests,
         feedback: action.feedback,
@@ -70,7 +72,7 @@ const reducer = (state = initialState, action) => {
         durationTotalNoExit: action.durationTotalNoExit,
         exitIntents: action.exitIntents,
         durationTotal: action.durationTotal,
-      })
+      }
     default:
       return state
   }
